2d5dcaa20263b7b32a4d7918ea028b59b34428bd
taica84 <taica_rules@yahoo.com>
2024-04-25 03:11:50 +0300

    edit hero

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -23,22 +23,22 @@ const items = [
     img: blogImg,
     link: 'https://nextjs-blog-taica07.vercel.app/',
   },
-  {
-    id: 3,
-    color: 'from-violet-300 to-purple-300',
-    title: 'Vanilla Book App',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
-    img: 'https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    link: 'https://lama.dev',
-  },
-  {
-    id: 4,
-    color: 'from-purple-300 to-red-300',
-    title: 'Spotify Music App',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
-    img: 'https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    link: 'https://lama.dev',
-  },
+  // {
+  //   id: 3,
+  //   color: 'from-violet-300 to-purple-300',
+  //   title: 'Vanilla Book App',
+  //   desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+  //   img: 'https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  //   link: 'https://lama.dev',
+  // },
+  // {
+  //   id: 4,
+  //   color: 'from-purple-300 to-red-300',
+  //   title: 'Spotify Music App',
+  //   desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+  //   img: 'https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  //   link: 'https://lama.dev',
+  // },
 ];
 
 const PortfolioPage = () => {
