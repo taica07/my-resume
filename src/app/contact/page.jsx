77a8ca1a4cc1ac8183b920e529cbf5e8d1d9bb2e
taica84 <taica_rules@yahoo.com>
2024-04-25 11:54:21 +0300

    responsive homepage

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -43,7 +43,7 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/3 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
           <div>
             {text.split('').map((letter, index) => (
               <motion.span
@@ -67,10 +67,11 @@ const ContactPage = () => {
           onSubmit={sendEmail}
           ref={form}
           // className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
-          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-6 lg:p-24"
+          className="h-3/4 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-6 lg:p-24"
         >
           <span>Dear Mihai,</span>
           <textarea
+            required
             autoFocus
             rows={6}
             className="bg-transparent border-b-2 border-b-black outline-none resize-none"
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 const Homepage = () => {
   return (
     <motion.div
-      className="h-full"
+      className="h-full overflow-hidden"
       initial={{ y: '-200vh' }}
       animate={{ y: '0%' }}
       transition={{ duration: 1 }}
@@ -19,11 +19,11 @@ const Homepage = () => {
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-6xl font-bold">
+          <h1 className="text-3xl md:text-6xl font-bold">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
           {/* DESC */}
-          <p className="md:text-xl">
+          <p>
             Welcome to my digital canvas, where innovation and creativity
             converge. With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
